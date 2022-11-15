1a2067bf6fca04ea9778a57d3e591988808dbf98
tlorence <tirone.lorence@gmail.com>
2022-11-15 22:31:16 +0530

    Inital_Commit

---
+++ Backend/src/main/java/com/ssd/app/SSD/SsdApplication.java
@@ -0,0 +1,13 @@
+package com.ssd.app.SSD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsdApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ssd/app/SSD/SsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssd.app.SSD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
