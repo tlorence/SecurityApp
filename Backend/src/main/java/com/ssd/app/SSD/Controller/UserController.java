d3c9ebe4bb523cf1ce7cbe2821b5fddc9f60203b
tlorence <tirone.lorence@gmail.com>
2022-11-16 23:00:57 +0530

    JWT

--- Backend/src/main/java/com/ssd/app/SSD/Controller/UserController.java
+++ Backend/src/main/java/com/ssd/app/SSD/Controller/UserController.java
@@ -1,2 +1,21 @@
-package com.ssd.app.SSD.Controller;public class UserController {
+package com.ssd.app.SSD.Controller;
+
+import com.ssd.app.SSD.Model.User;
+import com.ssd.app.SSD.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/add")
+    public String addUser(@RequestBody User user){
+        userRepository.save(user);
+        return "User Saved Successfully";
+    }
 }
--- Backend/src/main/java/com/ssd/app/SSD/Repositories/UserRepository.java
+++ Backend/src/main/java/com/ssd/app/SSD/Repositories/UserRepository.java
@@ -1,2 +1,7 @@
-package com.ssd.app.SSD.Repositories;public class UserRepository {
+package com.ssd.app.SSD.Repositories;
+
+import com.ssd.app.SSD.Model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
 }
